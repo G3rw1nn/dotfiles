fa4668a71d0b1876ba69e18971b35064e638d456
G3rw1nn <g3rw1n@protonmail.com>
2024-08-15 15:42:29 +0200

    fix icon starship

--- install_windows.ps1
+++ install_windows.ps1
@@ -11,7 +11,13 @@ scoop install extras/vscode
 scoop install extras/wsl-terminal
 scoop install extras/keepass
 scoop install extras/obsidian
+scoop install extras/rancher-desktop
 
 scoop bucket add nerd-fonts
 scoop install nerd-fonts/NerdFontsSymbolsOnly
 scoop install nerd-fonts/Hack-NF
+
+scoop bucket add nonportable
+scoop install nonportable/wireguard-np
+
+scoop install main/git
