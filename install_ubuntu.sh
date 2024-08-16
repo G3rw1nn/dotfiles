06c06e4f07d06e6b912de14bafdccb04bba9d323
G3rw1nn <g3rw1n@protonmail.com>
2024-08-16 12:32:50 +0200

    devbox install

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# update apt package list.
-apt update && apt upgrade
 
 #Install Starship https://starship.rs/guide/#%F0%9F%9A%80-installation
 curl -sS https://starship.rs/install.sh | sh
+
 
 # update apt package list.
 apt update
@@ -15,7 +14,7 @@ apt install stow
 stow .
 
 # https://www.jetify.com/devbox/docs/installing_devbox/
-# curl -fsSL https://get.jetify.com/devbox | bash
+curl -fsSL https://get.jetify.com/devbox | bash
 
 #Install ZSH
 apt install zsh
